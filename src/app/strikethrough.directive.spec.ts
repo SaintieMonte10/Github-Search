3fb64ecd13667f11bfe04abf7f66a8ee5602cf24
SaintieMonte10 <claremonte53@gmai.com>
2020-04-14 09:38:57 +0300

    added strikethrough

--- src/app/strikethrough.directive.spec.ts
+++ src/app/strikethrough.directive.spec.ts
@@ -6,3 +6,11 @@ describe('StrikethroughDirective', () => {
     expect(directive).toBeTruthy();
   });
 });
+import { StrikethroughDirective } from './strikethrough.directive';
+
+describe('StrikethroughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StrikethroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
