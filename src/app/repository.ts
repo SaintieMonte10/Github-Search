4d150eceddddc2919862a4db71073a4070770834
SaintieMonte10 <claremonte53@gmai.com>
2020-04-14 09:34:04 +0300

    date count

---
+++ src/app/repository.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+  today: number = Date.now();
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
