fd0f42d11dbeb1ff4928e7821a22ec7c147e5f96
SaintieMonte10 <claremonte53@gmai.com>
2020-04-14 09:32:38 +0300

    worked on app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AlertsService } from "./alert-service/alert.service";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserFormComponent } from "./user-form/user-form.component";
+import { UserComponent } from "./user/user.component";
+import { HttpClient } from "selenium-webdriver/http";
+import { StrikethroughDirective } from "./strikethrough.directive";
+
+import { RouterModule, Routes } from "@angular/router";
+import { DateCountPipe } from "./date-count.pipe";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NotFoundComponent } from './not-found/not-found.component';
+const routes: Routes = [
+  { path: "Home", component: UserComponent },
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserFormComponent,
+    UserComponent,
+    StrikethroughDirective,
+    
+    DateCountPipe,
+    NavbarComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [AlertsService], //add service to providers
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
